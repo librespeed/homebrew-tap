53a6676ad09dc4a76e7b29ced4f65ad316304656
Perflyst <mail@perflyst.de>
2020-03-11 08:30:11 +0100

    Fix version

--- Formula/librespeed-cli.rb
+++ Formula/librespeed-cli.rb
@@ -1,7 +1,7 @@
 class LibrespeedCli < Formula
   desc "Command-line client for LibreSpeed"
   homepage "https://github.com/librespeed/speedtest-cli"
-  url "https://github.com/librespeed/speedtest-cli/archive/v1.1.0.tar.gz"
+  url "https://github.com/librespeed/speedtest-cli/archive/v1.0.0.tar.gz"
   sha256 "e6a24c644aa599810396e10234d9f787f717a302c080cebaab1831be854f107e"
   head "https://github.com/librespeed/speedtest-cli.git"
 
