18030e5a541ec886e7d6154f35eb865f946489b2
Perflyst <mail@perflyst.de>
2020-03-11 09:34:38 +0100

    Update librespeed-cli to v1.0.1

--- Formula/librespeed-cli.rb
+++ Formula/librespeed-cli.rb
@@ -1,8 +1,8 @@
 class LibrespeedCli < Formula
   desc "Command-line client for LibreSpeed"
   homepage "https://github.com/librespeed/speedtest-cli"
-  url "https://github.com/librespeed/speedtest-cli/archive/v1.0.0.tar.gz"
-  sha256 "e6a24c644aa599810396e10234d9f787f717a302c080cebaab1831be854f107e"
+  url "https://github.com/librespeed/speedtest-cli/archive/v1.0.1.tar.gz"
+  sha256 "32040c0e788aef6078f68e7a987eeca1cf39774eb1a2a6793858a4d81cf37fe5"
   head "https://github.com/librespeed/speedtest-cli.git"
 
   bottle :unneeded
@@ -13,7 +13,7 @@ class LibrespeedCli < Formula
     build_time = Utils.popen_read("date -u '+%Y-%m-%d %H:%M:%S %Z' 2> /dev/null").chomp
     ldflags = ["-w -s",
                "-X \"librespeed-cli/defs.ProgName=librespeed-cli\"",
-               "-X \"librespeed-cli/defs.ProgVersion=#{version}\"",
+               "-X \"librespeed-cli/defs.ProgVersion=v#{version}\"",
                "-X \"librespeed-cli/defs.BuildDate=#{build_time}\""]
     system "go", "build", "-o", bin/"librespeed-cli", "-ldflags", ldflags.join(" "), "-trimpath", "main.go"
   end
